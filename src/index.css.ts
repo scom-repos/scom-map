15043b64ffdf2f74a86e63446d4bdc3719ba9936
celina-ijs <celina.dev@instajs.com>
2023-04-27 11:12:07 +0700

    Update to use scom-dapp-container

--- src/index.css.ts
+++ src/index.css.ts
@@ -1,72 +1,10 @@
 import { Styles } from '@ijstech/components';
 const Theme = Styles.Theme.ThemeVars;
 
-Styles.cssRule('#pnlImage', {
+Styles.cssRule('i-scom-map', {
   $nest: {
-    '.custom-img img': {
-      objectFit: 'cover',
-      objectPosition: 'center',
-      width: '100%',
-      height: '100%',
-      maxWidth: 'none',
-      maxHeight: 'none'
-    },
-    '#imgLink span': {
-      display: 'block'
-    },
-    '#edtLink input': {
-      border: `1px solid ${Theme.divider}`
-    },
-    ".angle": {
-      zIndex: '200',
-      position: 'absolute',
-      width: '30px',
-      height: '30px',
-      background: 'black',
-      clipPath: "polygon(0 0, 0 100%, 20% 100%, 20% 20%, 100% 20%, 100% 0)"
-    },
-    ".transform": {
-      transformOrigin: "left top"
-    },
-
-    ".angle-nw:hover": {
-      cursor: 'nw-resize',
-      background: 'blue'
-    },
-    ".angle-ne:hover": {
-      cursor: 'ne-resize',
-      background: 'blue'
-    },
-    ".angle-sw:hover": {
-      cursor: 'sw-resize',
-      background: 'blue'
-    },
-    ".angle-se:hover": {
-      cursor: 'se-resize',
-      background: 'blue'
-    },
-
-    ".angle-ne": {
-      transform: "rotate(90deg)"
-    },
-    ".angle-se": {
-      transform: "rotate(180deg)"
-    },
-    ".angle-sw": {
-      transform: "rotate(270deg)"
-    },
-
-    ".canvas": {
-      zIndex: '180',
-      position: 'absolute',
-      top: '0px',
-      left: '0px'
-    },
-    ".canvas-line": {
-      zIndex: '190',
-      position: 'absolute',
-      top: '0px',
-      left: '0px'
+    '#pnlModule': {
+      height: '100%'
     }
   }
 });
