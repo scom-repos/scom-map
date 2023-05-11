1c2815b81583ab635ee4f0880a8ec93c69f6e2c7
celina-ijs <celina.dev@instajs.com>
2023-05-11 14:42:13 +0700

    Add default data

---
+++ src/data.json.ts
@@ -0,0 +1,8 @@
+export default {
+  "apiKey": "AIzaSyDc7PnOq3Hxzq6dxeUVaY8WGLHIePl0swY",
+  "apiUrl": "https://www.google.com/maps/embed/v1/place",
+  "embeddedUrl": "https://maps.google.com/maps?hl=en&q={lat},{long}&t=h&z=14&ie=UTF8&iwloc=B&output=embed",
+  "defaultBuilderData": {
+    zoom: 15
+  }
+}
